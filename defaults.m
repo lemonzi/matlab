411b0eb35c204905650538ccd1e86e1b73864b50
Quim Llimona <ql@lemonzi.me>
2014-05-01 14:15:50 -0400

    defaults accepts (name,value) varargin inputs as cell, more matlab-like

--- defaults.m
+++ defaults.m
@@ -1,12 +1,29 @@
 function out = defaults(new, def)
-% DEFAULTS
+% DEFAULTS (new, default)
 % Copies missing fields in the new struct from defaults
+% New can also be a cell array. In that case, 
+%    - if it's empty, defaults are returned
+%    - if it has a single element, it's parsed as struct
+%    - else, the elements are parsed as (field,value) pairs
+% This is useful for parsing varargin without boilerplate
 
-    out = new;
+    if isstruct(new)
+        out = new;
+    else
+        if numel(new) == 0
+            out = def;
+            return;
+        elseif numel(new) == 1
+            out = new{1};
+        else
+            out = cell2struct(new(2:2:end), new(1:2:end));
+        end
+    end
+    
     fields = fieldnames(def);
 
     for i = 1:numel(fields)
-        if (~isfield(new, fields{i}))
+        if (~isfield(out, fields{i}))
             out.(fields{i}) = def.(fields{i});
         end
     end
